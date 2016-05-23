6b3f9567fb8c0e9cc1112a73d63cb61338a69a5e
Bob Brodt <bbrodt@redhat.com>
2016-05-23 07:59:10 -0600

    Added JSON read/write for tasks

--- uftasks/uftasks-showcase/uftasks-webapp/src/main/java/org/uberfire/client/screens/ProjectsPresenter.java
+++ uftasks/uftasks-showcase/uftasks-webapp/src/main/java/org/uberfire/client/screens/ProjectsPresenter.java
@@ -16,6 +16,7 @@
 
 package org.uberfire.client.screens;
 
+import javax.annotation.PostConstruct;
 import javax.enterprise.context.ApplicationScoped;
 import javax.enterprise.event.Event;
 import javax.enterprise.event.Observes;
@@ -23,6 +24,12 @@ import javax.enterprise.inject.Produces;
 import javax.inject.Inject;
 import javax.inject.Named;
 
+import org.jboss.errai.common.client.api.Caller;
+import org.jboss.errai.common.client.api.RemoteCallback;
+import org.jboss.errai.security.shared.api.identity.User;
+import org.uberfire.backend.vfs.Path;
+import org.uberfire.backend.vfs.PathFactory;
+import org.uberfire.backend.vfs.VFSService;
 import org.uberfire.client.annotations.WorkbenchPartTitle;
 import org.uberfire.client.annotations.WorkbenchPartView;
 import org.uberfire.client.annotations.WorkbenchScreen;
@@ -38,6 +45,14 @@ import org.uberfire.shared.model.Project;
 import org.uberfire.shared.model.Task;
 import org.uberfire.shared.model.TasksRoot;
 
+import com.google.gwt.core.client.GWT;
+import com.google.gwt.json.client.JSONArray;
+import com.google.gwt.json.client.JSONBoolean;
+import com.google.gwt.json.client.JSONObject;
+import com.google.gwt.json.client.JSONParser;
+import com.google.gwt.json.client.JSONString;
+import com.google.gwt.json.client.JSONValue;
+
 @ApplicationScoped
 @WorkbenchScreen(identifier = "ProjectsPresenter")
 public class ProjectsPresenter {
@@ -48,6 +63,7 @@ public class ProjectsPresenter {
 
         void addProject(Project project, boolean selected);
     }
+    private final static String DEFAULT_URI = "default://uftasks";
 
     @Inject
     private View view;
@@ -58,8 +74,12 @@ public class ProjectsPresenter {
     @Inject
     private Event<ProjectSelectedEvent> projectSelectedEvent;
 
-    @Produces
-    @Named("tasksRoot")
+    @Inject
+    private User user;
+
+    @Inject
+    protected Caller<VFSService> vfsServices;
+
     private TasksRoot tasksRoot = new TasksRoot();
 
     private Project activeProject = null;
@@ -75,8 +95,92 @@ public class ProjectsPresenter {
     }
 
     @Produces
+    @Named("tasksRoot")
     public TasksRoot tasksRoot() {
         return tasksRoot;
+    }
+
+    @PostConstruct
+    public void init() {
+        loadTasksRoot();
+    }
+
+    private void loadTasksRoot() {
+        String filename = "tasks.json";
+        String uri = DEFAULT_URI + "/" + user.getIdentifier() + "/" + filename;
+        Path path = PathFactory.newPath(filename, uri);
+
+        vfsServices.call(new RemoteCallback<String>() {
+            @Override
+            public void callback(final String response) {
+                TasksRoot newRoot = new TasksRoot();
+                JSONObject tasksRootJson = JSONParser.parseStrict(response).isObject();
+                JSONArray projectsJson = tasksRootJson.get("projects").isArray();
+                for (int pi=0; pi<projectsJson.size(); ++pi) {
+                    JSONObject pj = projectsJson.get(pi).isObject();
+                    Project p = new Project(pj.get("name").isString().stringValue());
+                    JSONArray foldersJson = pj.get("folders").isArray();
+                    for (int fi=0; fi<foldersJson.size(); ++fi) {
+                        JSONObject fj = foldersJson.get(fi).isObject();
+                        Folder f = new Folder(fj.get("name").isString().stringValue());
+                        JSONArray tasksJson = fj.get("tasks").isArray();
+                        for (int ti=0; ti<tasksJson.size(); ++ti) {
+                            JSONObject tj = tasksJson.get(ti).isObject();
+                            Task t = new Task(tj.get("name").isString().stringValue());
+                            t.setDone(tj.get("isDone").isBoolean().booleanValue());
+                            f.getChildren().add(t);
+                        }
+                        p.getChildren().add(f);
+                    }
+                    newRoot.getProjects().add(p);
+                }
+                tasksRoot = newRoot;
+                updateView();
+            }
+        }).readAllString(path);
+    }
+    
+    private void saveTasksRoot() {
+        JSONObject tasksRootJson = new JSONObject();
+        JSONArray projectsJson = new JSONArray();
+        int pi = 0;
+        for (Project p : tasksRoot.getProjects()) {
+            JSONObject pj = new JSONObject();
+            pj.put("name", new JSONString(p.getName()));
+            
+            JSONArray foldersJson = new JSONArray();
+            int fi = 0;
+            for (Folder f : p.getChildren()) {
+                JSONObject fj = new JSONObject();
+                fj.put("name", new JSONString(f.getName()));
+                foldersJson.set(fi++, fj);
+                
+                JSONArray tasksJson = new JSONArray();
+                int ti = 0;
+                for (Task t : f.getChildren()) {
+                    JSONObject tj = new JSONObject();
+                    tj.put("name", new JSONString(t.getName()));
+                    tj.put("isDone", JSONBoolean.getInstance(t.isDone()));
+                    tasksJson.set(ti++, tj);
+                }
+                fj.put("tasks", tasksJson);
+            }
+            pj.put("folders", foldersJson);
+            projectsJson.set(pi++, pj);
+        }
+        tasksRootJson.put("projects", projectsJson);
+        
+        final String content = tasksRootJson.toString();
+        String filename = "tasks.json";
+        String uri = DEFAULT_URI + "/" + user.getIdentifier() + "/" + filename;
+        Path path = PathFactory.newPath(filename, uri);
+
+        vfsServices.call(new RemoteCallback<Path>() {
+            @Override
+            public void callback(final Path response) {
+                GWT.log("Write Response: " + response);
+            }
+        }).write(path, content);
     }
     
     public void taskCreated(@Observes TaskCreated taskCreated) {
@@ -84,6 +188,7 @@ public class ProjectsPresenter {
             Folder folder = taskCreated.getFolder();
             Task task = taskCreated.getTask();
             folder.addChild(task);
+            saveTasksRoot();
             updateView();
         }
     }
@@ -91,12 +196,14 @@ public class ProjectsPresenter {
     public void taskDone(@Observes TaskDone taskDone) {
         Task task = taskDone.getTask();
         task.setDone(true);
+        saveTasksRoot();
         updateView();
     }
 
     public void folderCreated(@Observes FolderCreated folderCreated) {
         if (activeProject!=null) {
             activeProject.addChild(folderCreated.getFolder());
+            saveTasksRoot();
             updateView();
         }
     }
@@ -104,6 +211,7 @@ public class ProjectsPresenter {
     public void folderRemoved(@Observes FolderRemoved folderRemoved) {
         if (activeProject!=null) {
             activeProject.removeChild(folderRemoved.getFolder());
+            saveTasksRoot();
             updateView();
         }
     }
@@ -114,6 +222,7 @@ public class ProjectsPresenter {
 
     public void createNewProject(String projectName) {
         tasksRoot.getProjects().add(new Project(projectName));
+        saveTasksRoot();
         updateView();
     }
 
